d3f26c53a010428b8b2677349c579f7fddb5b7bc
Liban200 <125518353+Liban200@users.noreply.github.com>
2023-08-01 16:59:48 -0400

    Added functionality code.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -11,12 +11,29 @@ const initdb = async () =>
       console.log('jate database created');
     },
   });
-
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Putting data to database')
+  const editorDB = await openDB('jate', 1);
+  const tx = editorDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content});
 
-
+  const result = await request;
+  console.log('Data stored to database', result.value);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Getting data from database')
+  const editorDB = await openDB('jate', 1);
+  const tx = editorDB.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get(1);
+
+  const result = await request;
+  console.log('Data retrieved from database', result);
+  return result;
+};
 
 initdb();
+
